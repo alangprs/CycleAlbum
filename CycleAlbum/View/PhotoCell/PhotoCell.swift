c4290aaad49d137fb594221d42a8d0fb6ce33d31
will <alangprs1@gmail.com>
2021-12-11 00:51:49 +0800

    新增 要顯示的cell、照片

---
+++ CycleAlbum/View/PhotoCell/PhotoCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCell.swift
+//  CycleAlbum
+//
+//  Created by 翁燮羽 on 2021/12/11.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
