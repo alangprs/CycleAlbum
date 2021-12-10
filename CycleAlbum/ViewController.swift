919bfb41f69c5e016ecd201d74f5ed65dc35bc34
will <alangprs1@gmail.com>
2021-12-10 22:01:12 +0800

    Initial Commit

---
+++ CycleAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CycleAlbum
+//
+//  Created by 翁燮羽 on 2021/12/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CycleAlbumTests/CycleAlbumTests.swift
@@ -0,0 +1,33 @@
+//
+//  CycleAlbumTests.swift
+//  CycleAlbumTests
+//
+//  Created by 翁燮羽 on 2021/12/10.
+//
+
+import XCTest
+@testable import CycleAlbum
+
+class CycleAlbumTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
